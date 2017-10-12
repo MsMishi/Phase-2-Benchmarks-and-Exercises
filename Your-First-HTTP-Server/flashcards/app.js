836ae2b372ea27b3828561d52b06bf47bdcfdc1b
MsMishi <mcwcharles@gmail.com>
2017-10-12 11:59:43 -0700

    Add link functionality

--- Your-First-HTTP-Server/flashcards/app.js
+++ Your-First-HTTP-Server/flashcards/app.js
@@ -1,33 +1,46 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cookieParser= require('cookie-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser= require('cookie-parser');
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({extended: false }))
-app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(cookieParser());
 
-const mainRoutes = require('./routes')
-const cardRoutes = require('./routes/cards')
-const port = 3000
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+const port = 3000;
 
-app.use(mainRoutes)
-app.use('/cards', cardRoutes)
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.use((err, req, res, next) => {
-  res.locals.error = err
-  res.status(err.status)
-  res.render('error')
-  next(err)
-})
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+  next(err);
+});
+
+// app.use((req, res, next) => {
+//   const error = new Error('Invalid input');
+//   error.status = 400;
+//   next(error);
+// });
+//
+// app.use((error, req, res, next) => {
+//   res.locals.error = error;
+//   res.status(error.status);
+//   res.render('error');
+//   next(error);
+// });
 
 app.listen(port, () => {
-  console.log('The application is running on port localhost:3000')
-})
+  console.log('The application is running on port localhost:3000');
+});
